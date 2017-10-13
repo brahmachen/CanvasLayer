01d20cd73c336f7811f3745181778180f54d05d2
MasonChen <1570789797@qq.com>
2017-10-14 00:31:31 +0800

    add border to trace line

--- src/CanvasTrace.js
+++ src/CanvasTrace.js
@@ -8,25 +8,62 @@ function CanvasTrace(options) {
 CanvasTrace.prototype.draw = function() {
     var canvasLayer = new CanvasLayer({
         map: this._map,
-        update: update(this.trace)
+        update: update(this.options)
     })
 }
 
-function update(data) {
+function update(options) {
+    var _map = options.map;
+    var _trace = options.data;
     return function() {
         var ctx = this.canvas.getContext("2d");
         if (!ctx) {
             return;
         }
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.fillStyle = "rgba(50, 50, 255, 0.7)";
+
+        ctx.strokeStyle = "white";
+        // ctx.globalAlpha = 0.7;
+        ctx.lineWidth = options.width ? options.width * 1.5 : 9;
+        ctx.lineCap = "round";
         ctx.beginPath();
-        var p1 = map.pointToPixel(data[0]);
+        var p1 = _map.pointToPixel(_trace[0]);
         ctx.moveTo(p1.x, p1.y);
-        for (var i = 0, len = data.length; i < len; i++) {
-            var pixel = map.pointToPixel(data[i]);
+        for (var i = 0, len = _trace.length; i < len; i++) {
+            var pixel = _map.pointToPixel(_trace[i]);
             ctx.lineTo(pixel.x, pixel.y);
         }
         ctx.stroke();
+
+        ctx.strokeStyle = options.color || "rgba(50, 50, 255, 0.7)";
+        ctx.globalAlpha = 0.9;
+        ctx.lineWidth = options.width || 6;
+        ctx.lineCap = "round";
+        ctx.beginPath();
+        var p1 = _map.pointToPixel(_trace[0]);
+        ctx.moveTo(p1.x, p1.y);
+        var pixelArr = [];
+        for (var i = 0, len = _trace.length; i < len; i++) {
+            var pixel = _map.pointToPixel(_trace[i]);
+            pixelArr.push(pixel);
+            ctx.lineTo(pixel.x, pixel.y);
+        }
+        ctx.stroke();
+
+        // 轨迹上的方向箭头
+        // 像素点插值
+        console.log(JSON.stringify(pixelArr, null, '\t'));
+        for (var i in pixelArr) {
+            var item = pixelArr[i];
+            if (i == 0) continue;
+            var pre = pixelArr[i - 1];
+            var distance = Math.sqrt(Math.pow(Math.abs(pre.x - item.x), 2) +
+                Math.pow(Math.abs(pre.y - item.y), 2));
+            console.log(distance);
+        }
+        // 根据距离算出两个点之间需要插入几个点
+        // 然后插值让点均匀分布
+        // 最后截取出需要显示方向箭头的位置数组
+        // 画方向箭头，注意方向
     }
 }
--- trace-examples/trace.js
+++ trace-examples/trace.js
@@ -3,12 +3,18 @@
  */
 
 // 创建Map实例
-var map = new BMap.Map("map");
+var map1 = new BMap.Map("map");
 
-map.centerAndZoom(new BMap.Point(116.405706, 39.927773), 12); // 初始化地图,设置中心点坐标和地图级别
-map.enableScrollWheelZoom(); //启用滚轮放大缩小
+map1.centerAndZoom(new BMap.Point(116.405706, 39.927773), 12); // 初始化地图,设置中心点坐标和地图级别
+map1.enableScrollWheelZoom(); //启用滚轮放大缩小
 
 var canvasTrace = new CanvasTrace({
     data: traceArr,
-    map: map
+    map: map1,
+    color: "red",
+    width: 7
 })
+map1.setViewport([traceArr[0],
+    traceArr[traceArr.length - 1]
+])
+map1.zoomOut();
